fd56793b2a2991c4008bcfe428d1506ef4189a88
LukeSparg <lukesparg@hotmail.ca>
2020-03-02 16:09:54 -0800

    Created Chatbot class and made the main loop.

---
+++ Chatbot/src/com/company/Chatbot.java
@@ -0,0 +1,59 @@
+package com.company;
+import java.util.Scanner;
+
+public class Chatbot {
+	
+	Scanner in;
+	String listen;
+	String response;
+	
+	String custName;
+	String[][] responseData;
+	String[] farewellData = {"bye", "goodbye"};
+	
+	
+	public Chatbot() {
+		loadData();
+		in = new Scanner(System.in);
+		System.out.println("Hi, what's your name?");
+		custName = in.nextLine();
+		System.out.println("How can I help you " + custName + "?");
+	}
+	public boolean listenAndRespond() {
+		listen = in.nextLine();
+		listen = listen.toLowerCase();
+		
+		if(isFarewell()) {
+			//If they say bye or goodbye etc.
+			System.out.println("Goodbye.");
+			return false;
+		} else {
+			response = "I'm sorry, I do not understand. How else may I help you?";
+			getMatch();
+		}
+		System.out.println(response);
+		return true;
+	}
+	
+	boolean isFarewell() {
+		for(int i = 0; i < responseData.length; i++) {
+			if(listen.equals(farewellData[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	void getMatch() {
+		for(int i = 0; i < responseData.length; i++) {
+			if(listen.equals(responseData[i][0])) {
+				response = responseData[i][1];
+			}
+		}
+	}
+	
+	void loadData() {
+		//Only use lowercase
+	}
+	
+}
--- Chatbot/src/com/company/Main.java
+++ Chatbot/src/com/company/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
     	
-    	System.out.println("This is a test!");
+    	Chatbot bot = new Chatbot();
+    	boolean running = true;
+    	
+    	while(running) {
+    		running = bot.listenAndRespond();
+    	}
     	
     }
 }
