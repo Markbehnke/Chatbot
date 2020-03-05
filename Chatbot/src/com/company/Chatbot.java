be64eb86fba4fd0d94cf3e73ba73c8ebc6ef9353
muizzyusuf <43257447+muizzyusuf@users.noreply.github.com>
2020-03-05 10:05:34 -0800

    Updated chatbot.java to read from JSON file
    Updated chatbot class to read from JSON file. The chatbot now compares a users input to dialog prompts from a JSON file and also outputs the right response to a matching prompt from the JSON file to the user rather than reading from an array.

--- Chatbot/src/com/company/Chatbot.java
+++ Chatbot/src/com/company/Chatbot.java
@@ -1,102 +1,112 @@
 package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class Chatbot {
-	
+	Object obj;
 	Scanner in;
 	String listen;
 	String response;
-	
+
 	String custName;
-	String[][] responseData;
-	String[] farewellData = {"bye", "goodbye", "thanks", "thank you"};
-	
-	
+	String[] farewellData = { "bye", "goodbye", "thanks", "thank you" };
+
 	public Chatbot() {
 		in = new Scanner(System.in);
 		System.out.println("Hi, what's your name?");
 		custName = in.nextLine();
-		System.out.println("How can I help you " + custName + "?");
-		//loads the responseData array
-		loadData();
+		System.out.println("How may I help you " + custName + "?");
+
 	}
-	public boolean listenAndRespond() {
-		//get user input
+
+	public boolean listenAndRespond() throws FileNotFoundException, IOException, ParseException {//catch exceptions because a file is being read from
+		// get user input
 		listen = in.nextLine();
 		listen = listen.toLowerCase();
-		
-		//removes punctuation at the end of a question
-		char lastChar = listen.charAt(listen.length()-1);
-		if(lastChar == '.' || lastChar == '?') {
-			listen = listen.substring(0, listen.length()-1);
+
+		// removes punctuation at the end of a question as well as white spaces before and after
+		char lastChar = listen.charAt(listen.length() - 1);
+		if (lastChar == '.' || lastChar == '?') {
+			listen = listen.substring(0, listen.length() - 1).trim();
 		}
 		
-		if(isFarewell()) {
-			//If they say bye or goodbye etc.
-			System.out.println("Goodbye.");
-			return false;
-		} else {
-			//default response is not knowing
-			response = "I'm sorry, I do not understand. How else may I help you?";
-			//changes the response if it knows the answer
-			getMatch();
+		//Load up JSON file 
+		JSONParser jsonParser = new JSONParser();
+		FileReader reader = new FileReader("prompts.json");
+		boolean val = false;
+		Object obj = jsonParser.parse(reader);
+		
+		//Retrieve JSON file 
+		JSONArray responseData = (JSONArray) obj;
+		int len = responseData.size();//counts the number of dialog prompts in the JSON file that have been parsed.
+		
+		for (Object x : responseData) {
+			val = parseResponseData((JSONObject) x, listen, responseData.size());
+			len--;
+			//if true is returned and all the prompts in the JSON file have not been parsed,the user input matched a dialog prompt in the JSON file.
+			//then the chatbot response has been printed.
+			//Break the loop and wait for another user input. 
+			if (val == true) {
+				break;
+			} else if (len == 0) {
+				if (isFarewell()) {
+					//if all the prompts have been parsed and the input is goodbye, return false and and the conversation. 
+					System.out.println("Goodbye.");
+					return false;
+				} else {
+					//if all the prompts have been parsed and the input is not goodbye, tell the user to rephrase their input.
+					//break out of the loop to wait for another user input
+					System.out.println("I'm sorry, I do not understand. Do you mind rephrasing that?");
+					break;
+				}
+			}
 		}
-		System.out.println(response);
+
 		return true;
 	}
-	
-	boolean isFarewell() {
-		//checks to see if this should end the conversation
-		for(int i = 0; i < farewellData.length; i++) {
-			if(listen.equals(farewellData[i])) {
+
+	public static boolean parseResponseData(JSONObject prompt, String q, int len) {
+		//Check response data (JSON file) to see if user input q matches any dialog prompts.
+		//If true, print chatbot response from JSON file. If false return false. 
+		String response = "";
+		boolean val = false;
+		JSONObject intent = (JSONObject) prompt.get("intent");
+		JSONArray prompts = (JSONArray) intent.get("prompts");
+		val = checkPrompts(prompts, q);
+
+		if (val == true) {
+			response += (String) intent.get("response");
+			System.out.println(response);
+		}
+		return val;
+
+	}
+
+	public static boolean checkPrompts(JSONArray promptsArr, String q) {
+		//Check JSON array of prompts to see if user input q matches anyone of them, return true or false
+		for (Object x : promptsArr) {
+			String p = (String) x;
+			if (p.equals(q)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	void getMatch() {
-		//checks to see if this question is in the stored data
-		for(int i = 0; i < responseData.length; i++) {
-			if(listen.equals(responseData[i][0])) {
-				response = responseData[i][1];
+
+	boolean isFarewell() {
+		// checks to see if this should end the conversation
+		for (int i = 0; i < farewellData.length; i++) {
+			if (listen.equals(farewellData[i])) {
+				return true;
 			}
 		}
+		return false;
 	}
-	
-	void loadData() {
-		//All the question and response data
-		//lowercase for the all the [x][0] values
-		responseData = new String[30][2];
-		responseData[0][0] = "hello";
-		responseData[0][1] = "Hello, how can I help you " + custName + "?";
-		responseData[1][0] = "hey";
-		responseData[1][1] = "Hello, can I help you " + custName + "?";
-		responseData[2][0] = "how are you";
-		responseData[2][1] = "I'm doing great. What can I do for you?";
-		responseData[3][0] = "how can i place an order";
-		responseData[3][1] = "To order, create an account on our website first. You can then fill your basket with items and check out. If you have any special instructions/requests you can enter them there aswell.";
-		responseData[4][0] = "how fast will it take my order to arrive";
-		responseData[4][1] = "That depends. Did you use standard or express shipping?";
-		responseData[5][0] = "i used standard";
-		responseData[5][1] = "Your order will be delivered to you between 10-14 days from the day you order. If it has been over two weeks, check back with us.";
-		responseData[6][0] = "i used express";
-		responseData[6][1] = "Your order will be delivered to you between 2-5 days from the day you order. If it has been over a week, check back with us.";
-		responseData[7][0] = "what can i do if i get the wrong order";
-		responseData[7][1] = "take a picture of the items you received and send an email to freshfood@info.com with your order number and a representative will contact you able your options.";
-		responseData[8][0] = "what are my options";
-		responseData[8][1] = "We can pick up the non-perishable items from the order and issue a full refund. We can issue a partial refund if the issue is spoilt items. We could also offer a discount on your next order if you choose to keep the items.";
-		responseData[9][0] = "i have issues with my order";
-		responseData[9][1] = "I’m sorry to hear that. What is our order number?";
-		responseData[10][0] = "ordernum4";
-		responseData[10][1] = "I have located this order. What are your concerns?";
-		responseData[11][0] = "my order has not arrived yet and it’s been over two weeks";
-		responseData[11][1] = "I'm not seeing any issue on our side. To inquire about having the package send again please email us at freshfood@info.com";
-		responseData[12][0] = "i would like a refund";
-		responseData[12][1] = "I'm sorry to hear that. Please request a refund on the order under the order history tab.";
-		responseData[13][0] = "when will my item be in-stock";
-		responseData[13][1] = "Depending on the item, it can take 1-3 weeks to become available. Please check back frequently.";
-		responseData[14][0] = "i would like to cancel my order";
-		responseData[14][1] = "Please go to your Order History and cancel the order.";
-	}
+
 }
